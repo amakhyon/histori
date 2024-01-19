9578f859becc8c202b31780d543d1e35ed5bf638
amakhyon <a.m.makhyon@gmail.com>
2024-01-19 19:37:25 +0200

    added pdf parsing

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+// Require the upload middleware
+const upload = require('./upload');
+
+
+
+const ResumeParser = require('easy-resume-parser');
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/upload.html'));
+  });
+
+
+// Set up a route for file uploads
+app.post('/upload', upload.single('file'), (req, res) => {
+  // Handle the uploaded file
+  res.json({ message: 'File uploaded successfully!' });
+  
+ 
+
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
