5999315b6d726a1a2084a104ffdab78a6de40763
amakhyon <a.m.makhyon@gmail.com>
2024-01-28 03:56:23 +0200

    added links, names, organizations and more using NLP

--- parser.js
+++ parser.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 const pdf = require("pdf-parse");
+const nlp = require('compromise');
 
-let dataBuffer = fs.readFileSync("./uploads/1.pdf");
+
+var CVIndex = 0;
+for (var i=1; i < 15; i++){
+
+
+
+let dataBuffer = fs.readFileSync("./uploads/"+ i + ".pdf");
 
 //accepted formats are +971 or 00971 or (00971) or any other country code, otherwise it will be rejected!
-
 pdf(dataBuffer).then(function (data) {
+
 	// number of pages
 
 	// console.log(data.numpages);
@@ -29,8 +36,11 @@ pdf(dataBuffer).then(function (data) {
 
    
      //=============================================name===================================
-    var name = words[0]
-    console.log("Extracted name: ",name);
+     function extractNamesFromArray(array){
+      var name = array[0];
+      return name;
+     }
+   
     //=========================================================email=================================
     // Regular expression to match email addresses
 const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
@@ -60,22 +70,61 @@ function extractEmailsFromArray(array) {
 
     array.forEach((str) => {
       const matches = str.match(phoneRegex);
-      if (matches && matches[0].length > 5) {
+      if (matches && matches[0].length > 7) {
         extractedPhoneNumbers.push(matches[0].trim());
       }
     });
     return extractedPhoneNumbers;
   }
 
-  // Example usage:
+  function getWordsFromCV(array){
+    const wordRegex = /\w+/;
+    const extractedWords = [];
+
+    array.forEach((str) => {
+      const matches = str.match(wordRegex);
+      if (matches) {
+        extractedWords.push(matches[0].trim());
+      }
+    });
+    return extractedWords;
+  }
+  const extractedName = extractNamesFromArray(words);
   const extractedPhoneNumbers = extractPhoneNumbers(words);
-
-  console.log("Extracted phonenumber: ",extractedPhoneNumbers);
-
-  // Get the extracted emails
   const extractedEmails = extractEmailsFromArray(words);
 
-  // Output the result
+  const doc = nlp(AllResume);
+  const names = doc.people().out('array');
+  const phones = doc.phoneNumbers().out('array');
+  const emails = doc.emails().out('array');
+  const places = doc.places().out('array');
+  const links = doc.urls().out('array');
+  const organizations = doc.organizations().out('array');
+
+
+
+
+  console.log("               ----------- parsing CV " + CVIndex++ + "-----------");
+
+  console.log("Extracted Name: ", extractedName);
+  console.log("Extracted phonenumber: ",extractedPhoneNumbers);
   console.log("Extracted Emails: ", extractedEmails);
+
+
+
+  console.log("Found names:", names);
+  console.log("Found phones:", phones);
+  console.log("Found emails:", emails);
+  console.log("Found places:", places);
+  console.log("Found links:", links);
+  console.log("Found organizations:", organizations);
+
+  console.log("============================================================")
+
+
 });
+
+
+}//loop to loop all documents
+
 
